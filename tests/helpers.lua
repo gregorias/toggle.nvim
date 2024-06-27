6dac072feb6796ce39442bcd3a7657e8cfadf7ef
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-06-27 07:19:31 +0200

    test: add buffer-local option test

---
+++ tests/helpers.lua
@@ -0,0 +1,38 @@
+--- Test utilities.
+local M = {}
+
+--- Creates a buffer with the given content lines.
+---
+--- Also sets the buffer as the current buffer.
+---
+---@param content_lines
+---@return number The buffer number.
+M.create_buf = function(content_lines)
+  local buf = vim.api.nvim_create_buf(--[[listed]] false, --[[scratch]] true)
+  vim.api.nvim_buf_set_lines(
+    --[[buffer]]
+    buf,
+    --[[start]]
+    0,
+    --[[end]]
+    -1,
+    --[[strict_indexing]]
+    true,
+    content_lines
+  )
+  vim.cmd("buffer " .. buf)
+  return buf
+end
+
+--- Executes keys in the given mode.
+---
+--- This function also replaces termcodes like `<C-o>` into something that
+--- feedkeys can understand.
+--
+---@treturn nil
+M.execute_keys = function(feedkeys, mode)
+  local keys = vim.api.nvim_replace_termcodes(feedkeys, true, false, true)
+  vim.api.nvim_feedkeys(keys, mode, false)
+end
+
+return M
--- tests/toggle/toggle_spec.lua
+++ tests/toggle/toggle_spec.lua
@@ -1,7 +1,44 @@
+local test_helpers = require("tests.helpers")
 local toggle = require("toggle")
 
-describe("setup", function()
-  it("works", function()
-    toggle.setup({ register_keymaps = function() end })
+describe("toggle", function()
+  describe("setup", function()
+    it("works", function()
+      toggle.setup({ register_keymaps = function() end })
+    end)
+  end)
+
+  describe("register", function()
+    it("respects buffer-local options", function()
+      toggle.setup()
+      local hi_bufnr = test_helpers.create_buf({ "hi" })
+
+      local test_var = 0
+      toggle.register("t", {
+        name = "test",
+        get_state = function()
+          return test_var
+        end,
+        toggle_state = function()
+          test_var = 1 - test_var
+          return test_var
+        end,
+      }, { buffer = true })
+
+      -- Toggle test. Execute immediately and use remapping.
+      test_helpers.execute_keys("yot", "xm")
+			-- Check that the buffer-local option has been triggered.
+      assert.are.same(1, test_var)
+
+			-- Change buffer.
+      local world_bufnr = test_helpers.create_buf({ "world" })
+			-- Toggle test.
+      test_helpers.execute_keys("yot", "xm")
+			-- Check that the buffer-local option has not been triggered.
+      assert.are.same(1, test_var)
+
+			vim.api.nvim_buf_delete(world_bufnr, {})
+			vim.api.nvim_buf_delete(hi_bufnr, {})
+    end)
   end)
 end)
